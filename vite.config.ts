a5d6b9e8595b5397341b3c53c08facd4d9d2d730
jamesngn <103204978+jamesngn@users.noreply.github.com>
2024-05-25 19:00:09 +1000

    Update environment variables for local development

--- vite.config.ts
+++ vite.config.ts
@@ -2,14 +2,33 @@ import { defineConfig } from "vite";
 import path from "path";
 import react from "@vitejs/plugin-react-swc";
 
+// Ensure the environment variable is prefixed with VITE_
+
+// localhost
+
 export default defineConfig({
   server: {
     proxy: {
-      '/user-service': {  // This will match the base path of your backend URL
-        target: 'http://localhost',  // The host of your backend
-        changeOrigin: true,  // Needed to avoid host header issues
-        rewrite: path => path.replace(/^\/user-service/, '/user-service')  // Rewrite the URL path to target correctly
-      }
+      "/user-service": {
+        // This will match the base path of your backend URL
+        target: "http://192.168.1.139", // The host of your backend
+        changeOrigin: true, // Needed to avoid host header issues
+        rewrite: (path) => path.replace(/^\/user-service/, "/user-service"), // Rewrite the URL path to target correctly
+      },
+
+      "/recommendation-service": {
+        // This will match the base path of your backend URL
+        target: "http://192.168.1.139:8082",
+        changeOrigin: true, // Needed to avoid host header issues
+        rewrite: (path) =>
+          path.replace(/^\/recommendation-service/, "/recommendation-service"), // Rewrite the URL path to target correctly
+      },
+      "/place-service": {
+        // This will match the base path of your backend URL
+        target: "http://192.168.1.139:81",
+        changeOrigin: true, // Needed to avoid host header issues
+        rewrite: (path) => path.replace(/^\/place-service/, "/place-service"), // Rewrite the URL path to target correctly
+      },
     },
   },
   plugins: [react()],
