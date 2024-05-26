ad87bcaff8107646d0808bccab98ce20a97e1fc4
jamesngn <103204978+jamesngn@users.noreply.github.com>
2024-05-26 18:00:26 +1000

    chore: Add useGetPlacesByWorldCityIdQuery hook for fetching places by world city ID

--- src/api/hooks/place.hook.ts
+++ src/api/hooks/place.hook.ts
@@ -1,12 +1,21 @@
 import { useQuery } from "@tanstack/react-query";
 
-import { getPlaceById } from "@/api/place";
+import { getPlaceById, getPlacesByWorldCityId } from "@/api/place";
 
 export const useGetPlaceByIdQuery = (id: string) => {
   return useQuery({
     queryKey: [getPlaceById.name, id],
     queryFn: () => {
       return getPlaceById.fn(id);
+    },
+  });
+};
+
+export const useGetPlacesByWorldCityIdQuery = (worldCityId: number) => {
+  return useQuery({
+    queryKey: [getPlacesByWorldCityId.name, worldCityId],
+    queryFn: () => {
+      return getPlacesByWorldCityId.fn(worldCityId);
     },
   });
 };
