34c79c441b289890675951c72bea5eb473bd1507
jamesngn <103204978+jamesngn@users.noreply.github.com>
2024-05-26 18:00:19 +1000

    chore: Update import path for IPlaceData in getPlacesByWorldCityId

--- src/api/place.ts
+++ src/api/place.ts
@@ -1,7 +1,7 @@
 import { MutateFunction } from "@tanstack/react-query";
 
 import { placeServiceApi } from "./axiosInstance/placeServiceApi";
-import { IPlaceData } from "@/pages/places.page/interface";
+import { IPlaceData } from "@/pages/recommendations.page/interface";
 
 type TMutationConfig = {
   name: string;
@@ -23,6 +23,20 @@ export const getPlaceById = {
   fn: async (id: string) => {
     const res = await placeServiceApi<TResponse<IPlaceData>>({
       url: `/place/${id}`,
+      method: "GET",
+    });
+
+    const { data: responseData } = res.data;
+
+    return responseData;
+  },
+};
+
+export const getPlacesByWorldCityId = {
+  name: "getPlacesByWorldCityId",
+  fn: async (worldCityId: number) => {
+    const res = await placeServiceApi<TResponse<IPlaceData[]>>({
+      url: `/places/world_city/${worldCityId}`,
       method: "GET",
     });
 
