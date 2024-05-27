299dee7d5c926ab3d77e71326fa293015ec12ce9
jamesngn <103204978+jamesngn@users.noreply.github.com>
2024-05-27 12:26:34 +1000

    chore: Update updateUserPlacePreferences and calibrateUserPlacePreferences in user.ts

--- src/api/user.ts
+++ src/api/user.ts
@@ -1,35 +1,45 @@
-
 import { MutateFunction } from "@tanstack/react-query";
 
 import { userServiceApi } from "./axiosInstance/userServiceApi";
+import { WEBSITE_ACTIVITY_TYPE } from "@/shared/enums/biz.enum";
 
 type TMutationConfig = {
-    name: string;
-    fn: MutateFunction;
-  };
+  name: string;
+  fn: MutateFunction;
+};
 
 type TResponse<TData> = {
-    serverDateTime: string;
-    status: number;
-    code: number;
-    msg: string;
-    exception: null | any;
-    successful: boolean;
-    data: TData;
-  };
-
+  serverDateTime: string;
+  status: number;
+  code: number;
+  msg: string;
+  exception: null | any;
+  successful: boolean;
+  data: TData;
+};
 
 export const updateUserPlacePreferences = {
-    name: "updateUserPreferences",
-    fn: async (data: any) => {
-        const res = await userServiceApi<TResponse<any>>({
-            url:"/place_preference",
-            method: "PUT",
-            data,
-        });
+  name: "updateUserPreferences",
+  fn: async (data: any) => {
+    const res = await userServiceApi<TResponse<any>>({
+      url: "/place_preference",
+      method: "PUT",
+      data,
+    });
 
-        const { data: responseData } = res.data;
+    const { data: responseData } = res.data;
 
-        return responseData;
-    }
-}
+    return responseData;
+  },
+};
+
+export const calibrateUserPlacePreferences = {
+  name: "calibrateUserPreferences",
+  fn: async (data: any) => {
+    const res = await userServiceApi<TResponse<any>>({
+      url: "/place_preference/calibrate",
+      method: "PUT",
+      data,
+    });
+  },
+};
